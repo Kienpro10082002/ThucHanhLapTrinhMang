b359dae904ce7c8ba40bdd0971d2cabbee4c91f4
Kienpro10082002 <Zuka10082002@gmail.com>
2023-07-06 21:56:57 +0700

    half a practising 2

---
+++ ThucHanh2/src/main/java/com/mycompany/thuchanh2/ThucHanh2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thuchanh2;
+
+/**
+ *
+ * @author Admin
+ */
+public class ThucHanh2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
